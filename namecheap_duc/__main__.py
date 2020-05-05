5b9e3c3e0e1f235fcbbb5f734fd18b2f79c8d684
Evan Hastings <evanrh@disroot.org>
2020-05-05 01:23:57 -0400

    Moved driver into __main__ and made new class to not use requests

--- namecheap_duc/__main__.py
+++ namecheap_duc/__main__.py
@@ -1,4 +1,16 @@
-import driver
+import requests_client, argparse
 
-d = driver.Driver()
+class Driver():
+    '''
+        Basic object to handle creation of the client session
+    '''
+
+    def __init__(self):
+        self.client = requests_client.Requests_Client()
+
+    def start(self):
+        self.client.run()
+
+
+d = Driver()
 d.start()
